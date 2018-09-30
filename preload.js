f5ddc4663ab80b0fee619eba8d884dc851afeb3f
Vetleskrett <39460253+Vetleskrett@users.noreply.github.com>
2018-09-30 19:45:43 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,5 @@
 function preload() {
   unity = loadSound("sound/Unity.wav");
-  unity.setLoop(true);
-  unity.play();
-  unity.setVolume(0.5, 1);
   bite = loadSound("sound/bite.wav");
   hit = loadSound("sound/hit.wav");
   bell = loadSound("sound/bell.mp3");
